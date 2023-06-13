2045d6be65df3af3cff976183d1a615783d14ef6
lee14k <kmkovach@mtu.edu>
2023-06-12 20:39:30 -0400

    fix about routing

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,12 @@
 import Navbar from "./Navbar";
+import React from "react";
 
 export default function About () {
     return (
+        <div>
         <Navbar/>
 
         <h1>About</h1>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from '../../components/Navbar'
 import Header from '../../components/Header'
 import Mid from '../../components/Mid'
-
+import React from 'react'
 export default function Home() {
   return (
     <div >
